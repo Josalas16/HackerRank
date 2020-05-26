99763874b2895d02887fc2aadcb495f93ac96b18
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-26 14:55:21 -0600

    Clarified Instructions and Retested (Passed)

--- 30_Days_Of_Code/Day 1: Data Types.py
+++ 30_Days_Of_Code/Day 1: Data Types.py
@@ -16,7 +16,6 @@ i = 4
 d = 4.0
 s = 'HackerRank '
 # Declare second integer, double, and String variables
-
 # Read and save an integer, double, and String to your variables.
 userint = int(input())
 userdouble = float(input())
