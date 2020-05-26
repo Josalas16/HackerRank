adc14e1a57b4efdf4829378fad3b97bf1569fdfa
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-26 16:14:53 -0600

    Created Division

---
+++ Python Problems/Python: Division.py
@@ -0,0 +1,15 @@
+# Python: Division
+
+# Task
+# Read two integers ad print two lines. The first line should contain integer division,
+# a // b. The second line should contain float division, a /b.
+
+# Input Format
+# The first line contains the first integer, a. The second line contains the second integer, b.
+
+# Output Format
+# Print the two lines as described above.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
