e21530fdf6437c795ef8391009cf294da95dd41d
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-26 17:16:03 -0600

    Added Arithmetic Operations Section

---
+++ Python Problems/Arithmetic_Operators.py
@@ -0,0 +1,18 @@
+# Arithmetic Operators
+
+# Task
+# Read two integers from STDIN and print three lines where:
+# 1. The first line contains the sum of the two numbers.
+# 2. The second line contains the difference of the two numbers (first - second).
+# 3. The third line contains the product of the two numbers.
+
+# Input Format
+# The first line contains the first integer, a. The second line contains the second integer, b.
+
+# Output Format
+# Print the three lines as explained above
+
+# Sample
+# 3 + 2 -> 5
+# 3 - 2 -> 1
+# 3 * 2 -> 6
