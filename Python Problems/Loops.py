7eb761d97faf6551876fe722313520e64a132efd
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-26 17:04:23 -0600

    Created Loops Section

---
+++ Python Problems/Loops.py
@@ -0,0 +1,19 @@
+# Loops
+
+# Task
+# Read an N integer N. For all non-negative integers i < N, print i^2.
+
+# Constraints
+# 1 <= N <= 20
+
+# Output Format
+# Print N lines, one corresponding to each i
+
+# Sample input 0:
+# 5
+
+# Sample Output 0:
+# 0
+# 4
+# 9
+# 16
