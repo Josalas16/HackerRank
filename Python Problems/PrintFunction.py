e4e9b9d5c3836dbdd7405e15fe8798a47a26466a
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-26 20:06:53 -0600

    Created PrintFunction Challenge

---
+++ Python Problems/PrintFunction.py
@@ -0,0 +1,13 @@
+# Print Function
+
+# Read an integer N.
+# Without usng any string methods, tey to print the following:
+# 123 ... N
+# Note that "..." represents the values in between
+
+# Input Format
+# The first line contains an integer N.
+
+# Output Format
+# Output the answer as explained in the task
+
