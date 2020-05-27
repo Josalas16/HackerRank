71b7aa641c3e2ad25d84d0cb88d906c9ede8daa9
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-26 21:33:34 -0600

    Passes All Test Cases

--- Python Problems/WriteAFunction.py
+++ Python Problems/WriteAFunction.py
@@ -20,3 +20,16 @@
 # Output Format:
 # Output us taken care of by the template. Your function must return a boolean value (True/False)
 
+def is_leap(year):
+    leap = False
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
+        leap = True
+    elif (year % 100 == 0) and (year % 400 != 0):
+        leap = False
+    else:
+        leap = False
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
