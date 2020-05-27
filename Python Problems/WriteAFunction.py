f3f43314efb3b9467ce0da29ca45b70f5e55b151
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-26 21:18:05 -0600

    Created Write a Function Challenge

---
+++ Python Problems/WriteAFunction.py
@@ -0,0 +1,22 @@
+# Write a Function
+
+# Problem
+# We add a Leap Day on February 29, almost every four years. The leap day is an extra,
+# or intercalary day, and we add it to the shortest month of the year, February.
+# In the Gregorian calendar three criteria must be taken into account to identify leap years:
+# 1. The year can be divided by 4, is a leap year, unless:
+# 2. The year can be evenly divided by 100, it is NOT a leap year unless:
+# 3. The year is also evenly divisible by 400, Then it is a leap year.
+
+# This means that in the Gregorian calendar, the years 2000 and 2400 are leap years,
+# while 1800, 1900, 2100, 2200, 2300, and 2500 are NOT leap years.
+
+# Task
+# You are given the year, and you have to write a function to check if the year is leap or not
+
+# Input Format
+# Read y, the year that need to be checked.
+
+# Output Format:
+# Output us taken care of by the template. Your function must return a boolean value (True/False)
+
