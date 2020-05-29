18e0466f5cccc2613dc20d2d4bf2c0db7a3eaf1a
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-28 22:19:17 -0600

    Day 3: Intro To Conditional Statements Created

---
+++ 30_Days_Of_Code/Day 3: Intro To Conditional Statements.py
@@ -0,0 +1,28 @@
+# Day 3: Intro To Conditional Statements
+# Problem
+# Objective
+# In this challenge, we're getting started with conditional statements
+
+# Task
+# Given an integer, n, perform the following conditional actions:
+# 1. If n is odd, print Weird
+# 2. If n is even and in the inclusive range of 2 to 5, print Not Weird
+# 3. If n is even and in the inclusive range of 6 to 20, print Weird
+# 4. If n is even and greater than 20, print Not Weird
+# Complete the stub code provided in your editor to print whether or not n is weird
+
+# Input Format
+# A single line containing a positive integer, n
+
+# Constraints
+# 1 <= n <= 100
+
+# Output Format
+# Print Weird if the number is weird; otherwise, print Not Weird.
+
+# Explanation
+# Sample Case 0: n = 3
+# n is odd and odd numbers are weird, so we print Weird.
+# Sample Case 1: n = 24
+# n > 20 and n is even, so it isn't weird. Thus, we print Not Weird.
+
