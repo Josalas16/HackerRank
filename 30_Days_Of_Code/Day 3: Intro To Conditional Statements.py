4d4ec3f59f3058500946c5b133fa60475dd0b5aa
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-28 22:26:43 -0600

    Completed Day 3 and Passes all Test Cases

--- 30_Days_Of_Code/Day 3: Intro To Conditional Statements.py
+++ 30_Days_Of_Code/Day 3: Intro To Conditional Statements.py
@@ -34,3 +34,11 @@ import sys
 
 if __name__ == '__main__':
     N = int(input())
+    if(N % 2) != 0:
+        print("Weird")
+    elif(N % 2) == 0 and 2 <= N <= 5:
+        print("Not Weird")
+    elif(N % 2) == 0 and 6 <= N <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
