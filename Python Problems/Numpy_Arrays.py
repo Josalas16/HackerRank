a59a1df03e09327d11742921257cc77b1f2f0fb7
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-28 23:12:21 -0600

    Added Numpy Array Challenge

---
+++ Python Problems/Numpy_Arrays.py
@@ -0,0 +1,36 @@
+# Arrays
+# Problem
+# The NumPy (Numeric Python) package helps us manipulate large arrays and matrices of numeric data.
+# To use the Numpy module, we need to import it using:
+# import numpy as np
+
+# Arrays
+# A Numpy array is a grid of values. They are similar to lists, except that every element of an array
+# must be the same type
+
+# Example
+# import numpy as np
+# a = np.arraya([1,2,3,4,5])
+# print(a[1] # 2
+
+# b = np.array([1,2,3,4,5],float)
+# print(b[1]) # 2.0
+# In the above example, np.array() is used to convert a list into a Numpy array. The
+# second argument (float) can be used to set the type of array elements.
+
+# Task
+# You are given a space separated list of numbers.
+# Your task is to print a reversed NumPy array with the element type float.
+
+# Input Format
+# A single line of input containing space separated numbers.
+
+# Output Format
+# Print the reverse NumPy array with type float.
+
+# Sample Input
+# 1 2 3 4 - 8 -10
+
+# Sample Output
+# [-10. -8. 4. 3. 2. 1.]
+
