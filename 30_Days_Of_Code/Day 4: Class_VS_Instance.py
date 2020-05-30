2ce785033782624ad44a474ecff0f71a06de1c1c
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-29 22:02:32 -0600

    Simplified amIOld method

--- 30_Days_Of_Code/Day 4: Class_VS_Instance.py
+++ 30_Days_Of_Code/Day 4: Class_VS_Instance.py
@@ -46,7 +46,7 @@
 
 class Person:
     # Add some more code to run some checks on initialAge
-    def __init__(self,initialAge):
+    def __init__(self, initialAge):
         if initialAge < 0:
             self.age = 0
             print("Age is not valid, setting age to 0.")
@@ -57,7 +57,7 @@ class Person:
     def amIOld(self):
         if self.age < 13:
             print("You are young.")
-        elif self.age >= 13 and self.age < 18:
+        elif 13 <= self.age < 18:
             print("You are a teenager.")
         else:
             print("You are old.")
