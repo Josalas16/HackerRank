0111c423867e15029b1e33b15044f621c088936a
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-29 23:56:45 -0600

    Added Mod Power Challenge

---
+++ Python Problems/Mod_Power.py
@@ -0,0 +1,34 @@
+# Power - Mod Power
+
+# Problem
+# So far, we have only heard of Python's powers. Now, we will witness them!
+# Powers or exponents in Python can be calculated using the built-in power function.
+# Call the power function a^b as shown below
+# >>> pow(a,b) or a**b
+
+# It's also possible to calculate a^b mod m
+# pow(a,b,m)
+
+# This is very helpful in computations where you have to print the resultant & mod.
+# Note: Here, a and b can be floats or negatives, but, if a third argument is present, b cannot be aggressive.
+
+# Note: Python has a math module that has its own pow(). It takes two arguments and returns a float. Frankly speaking,
+# we will never use math.pow().
+
+# Task
+# You are given three integers: a, b, and m, respectively. Print two lines.
+# The first line should print the result of pow(a, b). The second line should print the result of pow(a, b, m).
+
+# Input Format
+# The first line contains a, the second line contains b, and the third line contains m.
+
+# Sample Input
+# 3
+# 4
+# 5
+
+# Sample Output
+# 81
+# 1
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
