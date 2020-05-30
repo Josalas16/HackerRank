da04c3bb14f32ded18bd97c1468cd40bcef897ef
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-30 00:51:42 -0600

    Completed Challenge (Passes all Test Cases)

--- Python Problems/String_Split_And_Join.py
+++ Python Problems/String_Split_And_Join.py
@@ -26,3 +26,12 @@
 # Sample Output
 # this-is-a-string
 
+def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
