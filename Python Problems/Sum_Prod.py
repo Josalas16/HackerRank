1dbdf6eaf6fd5276ff4012eeb906c9626cbd8bde
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-29 23:03:14 -0600

    Added Solution (Passes all Test Cases)

--- Python Problems/Sum_Prod.py
+++ Python Problems/Sum_Prod.py
@@ -26,3 +26,7 @@
 # The product of this sum = 24
 
 import numpy as np
+
+N, M = map(int, input().split())
+arr = np.array([input().split() for i in range(N)], int)
+print(np.prod(np.sum(arr, axis=0), axis=0))
