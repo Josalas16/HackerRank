c7cfafb82db78903d43f32d3832870ccefe72200
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-30 00:21:38 -0600

    Added Integer Sizes Challenge

---
+++ Python Problems/Integers_Come_In_All_Sizes.py
@@ -0,0 +1,26 @@
+# Integers Come In All Sizes
+# Problem
+# Integers in Python can be as big as the bytes in your machines  memory. There is no
+# limit in size as there is: 2^31 - 1 (c++ int) or 2^63 - 1 (C++ long long int).
+# As we know, the result of a^b grows really fast with increasing b
+# Let's do some calculations on very large integers
+
+# Task
+# Read four numbers, a, b, c and d, and print the result of a^b + c^d.
+
+# Input format
+# Integers a, b, c and d are given on four separate lines, respectively.
+
+# Output Format
+# Print the result of a^b + c^d on one line
+
+# Sample Input
+# 9
+# 29
+# 7
+# 27
+
+# Sample Output
+# 4710194409608608369201743232
+
+# Enter your code below. Read input from STDIN. Print output to STDOUT.
