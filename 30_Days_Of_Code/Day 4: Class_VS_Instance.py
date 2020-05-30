10b7fc9dc82728cb0ee62862a37c300ce417c7d7
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-29 21:52:51 -0600

    Added Starter Code

--- 30_Days_Of_Code/Day 4: Class_VS_Instance.py
+++ 30_Days_Of_Code/Day 4: Class_VS_Instance.py
@@ -44,3 +44,22 @@
 # You are old.
 # You are old.
 
+class Person:
+    # Add some more code to run some checks on initialAge
+    def __init__(self,initialAge):
+
+    # Do some computations in here and print out the correct statement to the console
+    def amIOld(self):
+
+    # Increment the age of the person in here
+    def yearPasses(self):
+
+t = int(input())
+for i in range(0,t):
+    age = int(input())
+    p = Person(age)
+    p.amIOld()
+    for j in range(0,3):
+        p.yearPasses()
+    p.amIOld()
+    print("")
