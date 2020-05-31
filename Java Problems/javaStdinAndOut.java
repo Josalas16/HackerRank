f7fc4c04617abc5a57164aaf0b831f3536ba03fe
Jaime Salas <JaimeOSalas@gmail.com>
2020-05-31 12:55:28 -0600

    Passes All Test Cases

--- Java Problems/javaStdinAndOut.java
+++ Java Problems/javaStdinAndOut.java
@@ -44,9 +44,14 @@ public class javaStdinAndOut {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-       
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        // used for closing scanner
+        scan.close();
 
         System.out.println(a);
-        
+        System.out.println(b);
+        System.out.println(c);
     }
 }
