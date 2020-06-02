cfa9c161e9e2f4ad6b3e2537a98a1030ea32cc85
Jaime Salas <JaimeOSalas@gmail.com>
2020-06-01 18:44:00 -0600

    Added Missing Code

--- 30_Days_Of_Code/Day 7: Arrays.py
+++ 30_Days_Of_Code/Day 7: Arrays.py
@@ -19,3 +19,14 @@
 # Sample Output
 # 2 3 4 1
 
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    arr = list(map(int, input().rstrip().split()))
+    print(" ".join(str(x) for x in arr[::-1]))
