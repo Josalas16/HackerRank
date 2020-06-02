375aaccc2d7c76b993129a3f64d3ba64b5949be5
Jaime Salas <JaimeOSalas@gmail.com>
2020-06-01 20:20:05 -0600

    Completed Challenge Passes All Test Cases

--- Java Problems/ifelse.java
+++ Java Problems/ifelse.java
@@ -33,6 +33,13 @@ public class ifelse {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N % 2 == 1 || (N >= 6 && N <= 20)){
+            System.out.println("Weird");
+        } else if (N % 2 == 0 && ((N >= 2 && N <= 5) || (N > 20))){
+            System.out.println("Not Weird");
+        }
+        scanner.close();
     }
 }
