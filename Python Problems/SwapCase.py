498425c2592525c9a4745d87e43e77dabcf3a6d9
Jaime Salas <JaimeOSalas@gmail.com>
2020-06-01 21:35:24 -0600

    Completed Challenge (Passes All Test Cases)

--- Python Problems/SwapCase.py
+++ Python Problems/SwapCase.py
@@ -17,3 +17,11 @@
 
 # Sample Output 0
 # hACKErANK.COM PRESENTS "pYTHONIST 2".
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
