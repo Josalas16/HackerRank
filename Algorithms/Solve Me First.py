dbd38f9df95199f57a83778e38772ba9fe38fa76
Jaime Salas <JaimeOSalas@gmail.com>
2020-06-01 20:56:35 -0600

    Added Algorithms section and first challenge

---
+++ Algorithms/Solve Me First.py
@@ -0,0 +1,20 @@
+# Solve me First
+
+# Function prototype:
+# int solveMeFirst(int a, int b); where
+
+# a is the first integer input
+# b is the second integer input
+
+# Return Values
+# sum of the above two integers
+
+# Sample Input
+# a = 2
+# b = 3
+
+# Sample Output
+# 5
+
+# Explanation
+# The sum of the two integers a and b is computed as: 2 + 3 = 5.
