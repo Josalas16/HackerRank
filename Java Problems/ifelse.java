ffb806f9cc12e4607c2cfdeb12dfbe7854805767
Jaime Salas <JaimeOSalas@gmail.com>
2020-06-01 20:13:49 -0600

    Added If else Challenge

---
+++ Java Problems/ifelse.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// Problem
+// In this challenge, we test your knowledge of using if-else conditional statements to automate
+// decision-making processes.
+
+// Task
+// Given an integer, n, perform the following conditional actions:
+// If n if odd, print Weird
+// If n is even and in the inclusive range of 2 to 5 print Not Weird
+// If n is even and in the inclusive range of 6 to 20 print Weird
+// If n is even and greater than 20, print Not Weird
+
+// Input Format
+// A single line containing a positive integer, n.
+
+// Output Format
+// Print Weird if the number is weird; otherwise, print Not Weird
+
+// Sample Input 0
+// 3
+
+// Sample Output 0
+// Weird
+
+public class ifelse {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
