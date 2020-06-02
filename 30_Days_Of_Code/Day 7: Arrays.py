8ee45d346f511dfa25a940c5753f669242e6ab02
Jaime Salas <JaimeOSalas@gmail.com>
2020-06-01 18:41:48 -0600

    Finished Day 7, Passes All Test Cases

---
+++ 30_Days_Of_Code/Day 7: Arrays.py
@@ -0,0 +1,21 @@
+# Day 7: Arrays
+
+# Problem
+# Objective
+# Task
+# Given an Array, A, of N integers, print A's elements in reverse order as a single line of space separated numbers.
+
+# Input Format
+# The first line contains an integer, N (the size of our array).
+# The second line contains N space-separated integers describing array A's elements.
+
+# Output Format
+# Print the elements of array A in reverse order as a singe line of space-separated numbers.
+
+# Sample Input
+# 4
+# 1 4 3 2
+
+# Sample Output
+# 2 3 4 1
+
